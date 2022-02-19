7877bae784750b370c515a38d871c4f89fea32e8
Abel kabeya <abelkofficial@gmail.com>
2022-02-19 12:02:05 +0200

    Adding doctor data from Admin Panel

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\HomeController;
+use App\Http\Controllers\AdminController;
 
 /*
 |--------------------------------------------------------------------------
@@ -23,3 +24,7 @@ Route::get('/home', [HomeController::class, 'redirect']);
 Route::middleware(['auth:sanctum', 'verified'])->get('/dashboard', function () {
     return view('dashboard');
 })->name('dashboard');
+
+Route::get('/add_doctor_view', [AdminController::class, 'addview']);
+
+Route::POST('/upload_doctor', [AdminController::class, 'upload']);
